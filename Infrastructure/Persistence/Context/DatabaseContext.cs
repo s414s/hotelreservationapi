3c98acc80a79893b98480dbd002f84b7015d4ee5
Alberto Salas <alsalasher@gmail.com>
2024-03-31 16:45:48 +0200

    add packages and string connection

---
+++ Infrastructure/Persistence/Context/DatabaseContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Persistence.Context;
+
+public class DatabaseContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DatabaseContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to postgres with connection string from app settings
+        options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+
+        //base.OnConfiguring(options);
+    }
+
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<Guest> Guests { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<User> Users { get; set; }
+}
