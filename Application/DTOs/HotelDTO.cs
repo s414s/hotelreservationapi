a94e7bdf3cde839e2d68a12130da07ae5e1f9813
Alberto Salas <alsalasher@gmail.com>
2024-04-02 17:20:32 +0200

    remove unused classes

--- Application/DTOs/HotelDTO.cs
+++ Application/DTOs/HotelDTO.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Entities;
+using Domain.Enum;
 
 namespace Application.DTOs;
 
@@ -6,16 +7,18 @@ public class HotelDTO
 {
     public long Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string City { get; set; } = string.Empty;
+    public Cities City { get; set; }
     public int NumberOfRooms { get; set; }
+    public IEnumerable<RoomDTO> Rooms { get; set; } = Enumerable.Empty<RoomDTO>();
     public static HotelDTO MapFromDomainEntity(Hotel hotel)
     {
         return new HotelDTO
         {
             Id = hotel.Id,
             Name = hotel.Name,
-            //City = hotel.Address?.City.ToString() ?? "no city found",
+            City = hotel.City,
             NumberOfRooms = hotel.Rooms.Count(),
+            Rooms = hotel.Rooms.Select(r => RoomDTO.MapFromDomainEntity(r)),
         };
     }
 }
--- Application/DTOs/SignupDTO.cs
+++ Application/DTOs/SignupDTO.cs
@@ -2,10 +2,7 @@
 
 public class SignupDTO
 {
-    public string Username { get; init; } = String.Empty;
     public string Name { get; init; } = String.Empty;
     public string Surname { get; init; } = String.Empty;
     public string Password { get; init; } = String.Empty;
-    public string RepeatPassword { get; init; } = String.Empty;    
-    public string DNI { get; init; } = String.Empty;
 }
