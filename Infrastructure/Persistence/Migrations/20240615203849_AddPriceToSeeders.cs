3d66a168cd454d6f00c668f0c507ca4e96acef1c
Alberto Salas <alsalasher@gmail.com>
2024-06-15 22:40:28 +0200

    Add price to seeders

---
+++ Infrastructure/Persistence/Migrations/20240615203849_AddPriceToSeeders.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriceToSeeders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Price",
+                value: 30m);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Price",
+                value: 30m);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "Price",
+                value: 30m);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "Price",
+                value: 30m);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 5L,
+                column: "Price",
+                value: 30m);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 6L,
+                column: "Price",
+                value: 30m);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 7L,
+                column: "Price",
+                value: 30m);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 8L,
+                column: "Price",
+                value: 30m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Price",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Price",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "Price",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "Price",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 5L,
+                column: "Price",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 6L,
+                column: "Price",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 7L,
+                column: "Price",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 8L,
+                column: "Price",
+                value: null);
+        }
+    }
+}
